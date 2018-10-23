ff88cc1ad58344119ec4ccf5545883aea0e796cb
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-23 11:56:52 -0400

    first commit

---
+++ src/main/java/com/lahey/springbootlesson201/HomeController.java
@@ -0,0 +1,24 @@
+package com.lahey.springbootlesson201;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/loadform")
+    public String loadFormPage(){
+
+        return "formtemplate";
+    }
+
+    @RequestMapping("/processform")
+    public String loadFromPage(@RequestParam("login") String login, Model model){
+
+        model.addAttribute("loginval", login);
+        return "confirm";
+    }
+
+}
---
+++ src/main/java/com/lahey/springbootlesson201/Springbootlesson201Application.java
@@ -0,0 +1,12 @@
+package com.lahey.springbootlesson201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlesson201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootlesson201Application.class, args);
+	}
+}
